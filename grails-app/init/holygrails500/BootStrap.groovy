88e2d60ddc65f786371d66b31a81a22ed33b2d31
Ken Kousen <ken.kousen@kousenit.com>
2021-10-24 16:31:51 -0400

    Skip bootstrap for test environment

--- grails-app/init/holygrails500/BootStrap.groovy
+++ grails-app/init/holygrails500/BootStrap.groovy
@@ -3,28 +3,29 @@ package holygrails500
 import com.kousenit.Castle
 import com.kousenit.GeocoderService
 import com.kousenit.Quest
+import grails.util.Environment
 
 class BootStrap {
     GeocoderService geocoderService
 
     def init = { servletContext ->
-//        if (Quest.count() == 0 && Castle.count() == 0) {
-//            Quest quest = new Quest(name: 'Seek the grail')
-//                    .addToTasks(name: 'Answer the bridgekeeper', priority: 4)
-//                    .addToTasks(name: 'Bring out your dead')
-//                    .addToTasks(name: 'Defeat the Black Knight')
-//                    .save(failOnError: true)
-//            Castle camelot = new Castle(name: 'Camelot', city: 'Marlborough', state: 'CT')
-//                    .addToKnights(title: 'Sir', name: 'Robin', quest: quest)
-//                    .addToKnights(title: 'King', name: 'Arthur', quest: quest)
-//                    .addToKnights(title: 'Sir', name: 'Lancelot', quest: quest)
-//            Castle espn = new Castle(name: 'ESPN', city: 'Bristol', state: 'CT')
-//            Castle yale = new Castle(name: 'Yale', city: 'New Haven', state: 'CT')
-//
-//            [camelot, espn, yale].collect {
-//                geocoderService.fillInLatLng(it).save(failOnError: true)
-//            }
-//        }
+        if (Environment.current != Environment.TEST && Quest.count() == 0) {
+            Quest quest = new Quest(name: 'Seek the grail')
+                    .addToTasks(name: 'Answer the bridgekeeper', priority: 4)
+                    .addToTasks(name: 'Bring out your dead')
+                    .addToTasks(name: 'Defeat the Black Knight')
+                    .save(failOnError: true)
+            Castle camelot = new Castle(name: 'Camelot', city: 'Marlborough', state: 'CT')
+                    .addToKnights(title: 'Sir', name: 'Robin', quest: quest)
+                    .addToKnights(title: 'King', name: 'Arthur', quest: quest)
+                    .addToKnights(title: 'Sir', name: 'Lancelot', quest: quest)
+            Castle espn = new Castle(name: 'ESPN', city: 'Bristol', state: 'CT')
+            Castle yale = new Castle(name: 'Yale', city: 'New Haven', state: 'CT')
+
+            [camelot, espn, yale].collect {
+                geocoderService.fillInLatLng(it).save(failOnError: true)
+            }
+        }
     }
 
     def destroy = {
