689fd2b325996e53c84519adc4c83131e63ea66f
Sergio del Amo <sergio.delamo@softamo.com>
2021-11-17 11:35:26 +0100

    use gorm data services in bootstrap

--- grails-app/init/holygrails500/BootStrap.groovy
+++ grails-app/init/holygrails500/BootStrap.groovy
@@ -1,29 +1,30 @@
 package holygrails500
 
 import com.kousenit.Castle
-import com.kousenit.GeocoderService
+import com.kousenit.CastleService
 import com.kousenit.Quest
-import grails.gorm.transactions.Transactional
+import com.kousenit.QuestService
 import grails.util.Environment
 
-@Transactional
 class BootStrap {
-
+    QuestService questService
+    CastleService castleService
     def init = { servletContext ->
-        if (Environment.current != Environment.TEST && Quest.count() == 0) {
-            Quest quest = new Quest(name: 'Seek the grail')
+        if (Environment.current != Environment.TEST && questService.count() == 0) {
+            Quest quest = questService.save(new Quest(name: 'Seek the grail')
                     .addToTasks(name: 'Answer the bridgekeeper', priority: 4)
                     .addToTasks(name: 'Bring out your dead')
-                    .addToTasks(name: 'Defeat the Black Knight')
-                    .save(failOnError: true)
-            Castle camelot = new Castle(name: 'Camelot', city: 'Marlborough', state: 'CT')
-                    .addToKnights(title: 'Sir', name: 'Robin', quest: quest)
-                    .addToKnights(title: 'King', name: 'Arthur', quest: quest)
-                    .addToKnights(title: 'Sir', name: 'Lancelot', quest: quest)
-            Castle espn = new Castle(name: 'ESPN', city: 'Bristol', state: 'CT')
-            Castle yale = new Castle(name: 'Yale', city: 'New Haven', state: 'CT')
-
-            [camelot, espn, yale].collect {it.save(failOnError: true) }
+                    .addToTasks(name: 'Defeat the Black Knight'))
+            for (Castle castle : [
+                    new Castle(name: 'Camelot', city: 'Marlborough', state: 'CT')
+                            .addToKnights(title: 'Sir', name: 'Robin', quest: quest)
+                            .addToKnights(title: 'King', name: 'Arthur', quest: quest)
+                            .addToKnights(title: 'Sir', name: 'Lancelot', quest: quest),
+                    new Castle(name: 'ESPN', city: 'Bristol', state: 'CT'),
+                    new Castle(name: 'Yale', city: 'New Haven', state: 'CT')
+            ]) {
+                castleService.save(castle)
+            }
         }
     }
 
